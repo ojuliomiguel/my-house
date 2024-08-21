813be027ac6958b019d59bac0d6972ca31a1c064
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2024-08-21 12:49:41 -0300

    refact: fix calculate strategy

--- src/main/java/com/lobster/myhouse/domain/Scoring/MyPopularHouseScoring.java
+++ src/main/java/com/lobster/myhouse/domain/Scoring/MyPopularHouseScoring.java
@@ -6,23 +6,37 @@ import com.lobster.myhouse.domain.Dependent;
 
 public class MyPopularHouseScoring implements Scoring {
 
+    private int MAX_INCOME = 1500;
+    private int INCOME_UP_TO = 900;
+
     @Override
     public double caculate(double totalIncome, List<Dependent> dependents) {
         double score = 0;
-        if (totalIncome <= 900) {
+        if (this.hasIncomeOutsideOfRange(totalIncome)) {
+            return score;
+        }
+        if (totalIncome <= this.INCOME_UP_TO) {
             score += 5;
-        } else if (totalIncome <= 1500) {
+        } else if (totalIncome <= this.MAX_INCOME) {
             score += 3;
         }
-        long eligibleDependents = dependents.stream()
+        long familiesUpToThreeDependents = dependents.stream()
+                .filter(dependent -> dependent.getAge() <= 18)
+                .count();
+
+        long familiesWithOneOrTwoDependents = dependents.stream()
                 .filter(dependent -> dependent.getAge() < 18)
                 .count();
-        if (eligibleDependents >= 3) {
+        if (familiesUpToThreeDependents >= 3) {
             score += 3;
-        } else if (eligibleDependents >= 1) {
+        } else if (familiesWithOneOrTwoDependents > 0 && familiesWithOneOrTwoDependents <= 2) {
             score += 2;
         }
         return score;
+    }
+
+    private boolean hasIncomeOutsideOfRange(double totalIncome) {
+        return totalIncome > this.MAX_INCOME;
     }
 
 }
