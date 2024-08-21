1ffd5f916973eea200372e1463154117754d1184
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2024-08-20 22:22:50 -0300

    feat: manage ListEligibleFamiliesUseCase dependecy

--- src/main/java/com/lobster/myhouse/infrastructure/IoC/UseCaseProvider.java
+++ src/main/java/com/lobster/myhouse/infrastructure/IoC/UseCaseProvider.java
@@ -1,10 +1,14 @@
 package com.lobster.myhouse.infrastructure.IoC;
 
 import com.lobster.myhouse.application.contracts.usecase.UseCaseContract;
+import com.lobster.myhouse.application.usecases.listEligibleFamilies.EligibleFamiliesUseCaseInput;
+import com.lobster.myhouse.application.usecases.listEligibleFamilies.EligibleFamiliesUseCaseOutput;
+import com.lobster.myhouse.application.usecases.listEligibleFamilies.ListEligibleFamiliesUseCase;
 import com.lobster.myhouse.application.usecases.registerFamilies.RegisterFamiliesUseCase;
 import com.lobster.myhouse.application.usecases.registerFamilies.RegisterFamiliesUseCaseInput;
 import com.lobster.myhouse.application.usecases.registerFamilies.RegisterFamiliesUseCaseOutput;
 import com.lobster.myhouse.domain.Scoring.Scoring;
+import com.lobster.myhouse.domain.gateway.ListFamiliesGateway;
 import com.lobster.myhouse.domain.gateway.RegisterFamiliesGateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +22,11 @@ public class UseCaseProvider {
     public UseCaseContract<List<RegisterFamiliesUseCaseInput>, List<RegisterFamiliesUseCaseOutput>> registerFamiliesUseCase(
             RegisterFamiliesGateway registerFamiliesGateway, Scoring scoring) {
         return new RegisterFamiliesUseCase(registerFamiliesGateway, scoring);
+    }
+
+    @Bean
+    public UseCaseContract<EligibleFamiliesUseCaseInput, List<EligibleFamiliesUseCaseOutput>> listEligibleFamiliesUseCase(
+            ListFamiliesGateway listFamiliesGateway) {
+        return new ListEligibleFamiliesUseCase(listFamiliesGateway);
     }
 }
